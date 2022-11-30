3e36023613ba035f9b551c054ba5e8389670a1c9
Zoltán Kovács <zoltan@geogebra.org>
2022-11-30 11:25:07 +0100

    Remove debug info

--- wprover/CMisc.java
+++ wprover/CMisc.java
@@ -364,7 +364,6 @@ public class CMisc {
                     full_screen = Boolean.parseBoolean(m2);
                 } else if (m1.equalsIgnoreCase("Language")) {
                     lan = m2;
-                    System.out.println("m2= "+m2);
                 } else if (m1.equalsIgnoreCase("Alpha")) {
                     float f = Float.parseFloat(m2);
                     fillac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, f);
