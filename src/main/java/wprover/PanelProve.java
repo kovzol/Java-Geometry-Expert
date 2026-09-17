174a5c104a55d7a68165f131031c9d52e58b57fb
vadgoblin@arch <vadgoblin@github.com>
2026-09-17 18:04:33 +0200

    fix url in graphvizNode

--- src/main/java/wprover/PanelProve.java
+++ src/main/java/wprover/PanelProve.java
@@ -1703,11 +1703,8 @@ public class PanelProve extends JTabbedPane implements ChangeListener {
                 tooltipstring += "\n" + getLanguage(exstring);
             }
             String rulestring = GExpert.getTranslationViaGettext("Rule {0}", rule + "");
-            String url = "";
-            // For some reason, this does not work, we get
-            // an error org.apache.batik.dom.util.SAXIOException: Invalid byte 1 of 1-byte UTF-8 sequence.
-            // FIXME.
-            // String url = "https://github.com/kovzol/Java-Geometry-Expert/blob/master/src/main/resources/docs/web/Rule" + rule + ".jpg?raw=true";
+
+            String url = BuildInfo.getRepoUrl() + "/blob/master/src/main/resources/docs/web/Rule" + rule + ".jpg?raw=true";
             n = Node.builder().shape(s).table(table().border(0).bgColor(c).
                     cellBorder(0).cellSpacing(0).cellPadding(6).href(url).tooltip(tooltipstring).
                     tr(td().color(c).text(co.getNo() + ") " + co.getText())).
